32d2e7333f3f4578dd1ee6247bf9013f3a90f850
aoyagikouhei <aoyagi.kouhei@gmail.com>
2012-10-10 08:25:05 +0900

    modify singleton to simple

--- lib/log4r/fluent_outputter.rb
+++ lib/log4r/fluent_outputter.rb
@@ -4,19 +4,20 @@ require 'fluent-logger'
 
 module Log4r
   class FluentOutputter < Log4r::Outputter
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
     attr_accessor :tag
+    attr_accessor :log
   
     def initialize(_name, hash={})
       super(_name, hash)
       @tag = hash[:tag] || hash["tag"] || 'test'
       host = hash[:host] || hash["host"] || 'localhost'
       port = hash[:port] || hash["port"] || 24224
-      ::Fluent::Logger::FluentLogger.open(nil, host: host, port: port.to_i)
+      @log = Fluent::Logger::FluentLogger.new(nil, host: host, port: port.to_i)
     end
   
     def canonical_log(logevent)
-      ::Fluent::Logger.post(@tag, {l: logevent.level, d: logevent.data})
+      @log.post(@tag, {l: logevent.level, d: logevent.data})
     end
   end
 end
